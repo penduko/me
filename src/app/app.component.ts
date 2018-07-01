cac323d1ce3a3e5252852a1a23e041783f0ba84b
penduko <clydebryan.ferrolino@gmail.com>
2018-07-01 10:48:00 +0800

    added multiple themes and footer layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
 import { OverlayContainer } from '@angular/cdk/overlay';
 
 @Component({
@@ -6,9 +6,55 @@ import { OverlayContainer } from '@angular/cdk/overlay';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my profile';
-  constructor(overLayContainer: OverlayContainer) {
-    // overLayContainer.getContainerElement().classList.add('indigo-pink');
+  fillerContent = Array(50)
+    .fill(0)
+    .map(() =>
+      `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+       labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+       laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+       voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+       cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
+    );
+
+  @HostBinding('class') componentCssClass;
+
+  themes = [
+    {value: 'black-theme', label: 'Dark'},
+    {value: 'default-theme', label: 'Blue'},
+    {value: 'light-theme', label: 'Light'},
+    {value: 'nature-theme', label: 'Nature'}
+  ];
+
+  DEFAULT_THEME = 'default-theme';
+
+  constructor(private overlayContainter: OverlayContainer) {
+
+    // set default for overlay component
+    this.overlayContainter.getContainerElement().classList.add(this.DEFAULT_THEME);
+  }
+
+  ngOnInit() {
+
+    // set default theme
+    this.componentCssClass = this.DEFAULT_THEME;
+  }
+
+  changeTheme(theme: string) {
+
+    // set our template theme
+    this.componentCssClass = theme;
+
+    // remove first the current theme in our overlay components
+    // before changing the for the new theme
+    const classList = this.overlayContainter.getContainerElement().classList;
+    const toRemove = Array.from(classList).filter((item: string) =>
+      item.includes('-theme')
+    );
+    if (toRemove.length) {
+      classList.remove(...toRemove);
+    }
+    classList.add(theme);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,21 +8,29 @@ import {
   MatIconModule,
   MatListModule,
   MatTooltipModule,
-  MatCardModule
+  MatCardModule,
+  MatMenuModule,
+  MatSlideToggleModule
 } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MainNavComponent } from './main-nav/main-nav.component';
-import { TestComponent } from './test/test.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+
+// Add an icon to the library for convenient access in other components
+library.add(fas, fab);
 
 @NgModule({
-  declarations: [AppComponent, MainNavComponent, MainNavComponent,
-    TestComponent
+  declarations: [AppComponent
 ],
   imports: [
     BrowserModule,
+
+    // material
     MatCheckboxModule,
     MatToolbarModule,
     MatButtonModule,
@@ -32,7 +40,12 @@ import { TestComponent } from './test/test.component';
     MatIconModule,
     MatListModule,
     MatTooltipModule,
-    MatCardModule
+    MatCardModule,
+    MatMenuModule,
+    MatSlideToggleModule,
+
+    // fontawesome
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
