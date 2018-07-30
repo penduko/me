04344b8e1e74f0e2455ecb16125912c961349bfe
penduko <clydebryan.ferrolino@gmail.com>
2018-07-30 17:16:12 +0800

    update production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,18 @@
+declare var require: any;
+const packageJson = require('../../package.json');
+
 export const environment = {
-  production: true
+  production: true,
+  versions: {
+    app: packageJson.version,
+    angular: packageJson.dependencies['@angular/core'],
+    ngrx: packageJson.dependencies['@ngrx/store'],
+    material: packageJson.dependencies['@angular/material'],
+    bootstrap: packageJson.dependencies.bootstrap,
+    rxjs: packageJson.dependencies.rxjs,
+    fontAwesome:
+      packageJson.dependencies['@fortawesome/fontawesome-svg-core'],
+    angularCli: packageJson.devDependencies['@angular/cli'],
+    typescript: packageJson.devDependencies['typescript']
+  }
 };
